f4f5e25dabd7088985b3a8cb834811f899f096d1
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-04-04 09:12:30 -0700

    Initial Commit

---
+++ StarterTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarterTemplate
+//
+//  Created by Prudhvi Gadiraju on 4/4/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
